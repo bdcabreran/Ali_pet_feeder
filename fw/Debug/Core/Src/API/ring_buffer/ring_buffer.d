e26e35b7f9ab4e6909d9aff1fcbc98f0a1f8b6e4
bdcabreran <bdcabreran@unal.edu.co>
2022-06-17 17:00:20 -0500

    Develop
    removing Debug files

--- fw/Debug/Core/Src/API/ring_buffer/ring_buffer.d
+++
@@ -1,4 +0,0 @@
-Core/Src/API/ring_buffer/ring_buffer.o: \
- ../Core/Src/API/ring_buffer/ring_buffer.c \
- ../Core/Inc/API/ring_buffer/ring_buffer.h
-../Core/Inc/API/ring_buffer/ring_buffer.h:
--- fw/Debug/Core/Src/API/ui_pictures/static_background.d
+++
@@ -1,2 +0,0 @@
-Core/Src/API/ui_pictures/static_background.o: \
- ../Core/Src/API/ui_pictures/static_background.c
