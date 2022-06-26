51800574d1e678d129bb1f7dd6bead3aaa8f7f79
Mauro R <emauriciorg@gmail.com>
2022-06-26 11:42:06 -0500

    feat : adc convertion was added under battery api

---
+++ fw/Core/Inc/API/battery/battery.h
@@ -0,0 +1,27 @@
+/**
+ * @file battery.h
+ * @author Mauro Rios (emauriciorg@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-06-26
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <stdint.h>
+
+#define ADC_MAX_VOLTAGE_VDD 3300
+#define ADC_TIMEOUT 100
+//#define ADC_CALIBRATION_ENABLE
+#define BATTERY_VOLTAGE_MV_FULL 11100
+#define BATTERY_R1 910000
+#define BATTERY_R2 240000
+#define BATTERY_RT (BATTERY_R1+BATTERY_R2)
+#define BATTERY_R2_MAX_MV_VOLTAGE 2316
+#define BATERY_VOLTAGE_CALC(voltage_r2) (voltage_r2*(BATTERY_RT/BATTERY_R2))
+
+
+void battery_init(void);
+
+uint16_t battery_read_voltage(void);
